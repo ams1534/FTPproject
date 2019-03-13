29f36ba7ca94ee198f4e70bf1b07d54c0eab80da
ams1534 <48520214+ams1534@users.noreply.github.com>
2019-03-13 08:29:40 -0500

    Update FTP_ConnectComputers.py

--- FTP_ConnectComputers.py
+++ FTP_ConnectComputers.py
@@ -1,46 +1,49 @@
 import socket 
 import pickle
-import optparse #This is used to parse console input -TS
+import optparse                                         # This is used to parse console input 
 import string
 # import threading                                      # used in server 
-# get input from user 
 
 #def cd(newDirectory, socket):
 
 
 def ls(socket):
-    socket.send(str.encode('LS'))                       # sends the requested commandInput in bytes
-    data = socket.recv(1024)
-    bytesRecv = pickle.loads(data)
-         
-    print('Recv')
+    cmd = pickle.dumps('LS')
+    socket.send(cmd)                                    # sends the requested commandInput in bytes
+    data = socket.recv(1024)                            # receives data from command
+    bytesRecv = pickle.loads(data)                      # pickle "decodes" the pickeled file dumped by the server                     
     return bytesRecv
 
 def dir(newDirectory, socket):
-    socket.send(str.encode('LS'))                       # sends the requested file name in bytes
-    dirc = socket.recv(1024)                            # accepts anything sent by server 
-    return dirc.decode('utf-8') 
+    cmd = pickle.dumps('DIR')
+    socket.send(cmd)                                    # sends the requested file name in bytes
+    data = socket.recv(1024)                            # receives data from command
+    bytesRecv = pickle.loads(data)                      # pickle "decodes" the pickeled file dumped by the server     
+    return bytesRecv
 
-def get(socket):                                        
+def get(socket):    
+    cmd = pickle.dumps('GET')
+    socket.send(cmd)                                    # sends the requested file name in bytes
     filename = input("filename?")                       # gets file desired from client
     if filename != 'quit':                              # 'q' is the indication that the client would like to quit and no longer get a file
-        socket.send(str.encode(filename))               # sends the requested file name in bytes
+        filename = pickle.dumps(filename)
+        socket.send(filename)                           # sends the requested file name in bytes
         data = socket.recv(1024)                        # accepts anything sent by server         low key limited to 1018 size file       
-        data = data.decode('utf-8')                     # special decode from the string 
+        data = pickle.loads(data)                       # special decode from the string 
         if data[:6] == 'EXISTS':
             filesize = data[6:]
             message = input("File exists, " + filesize + "Bytes, download? (Y/N)")
             if message == 'Y':                          # do a more specific check to verify that the user is saying yes i.e Y, y, yes, Yes, etc.
-                socket.send(str.encode('OK'))           # sends back string response
+                socket.send(pickle.dumps('OK'))           # sends back string response
                 f = open('new' + filename, 'wb')        # makes file with the word new infront 
                 data = socket.recv(1024)
                 totalRecv = len(data)
                 f.write(data)
                 while totalRecv < int(filesize):
-                    data = set.recv(1024)
-                    totalRecv += len(data)
+                    data = set.recv(1024)               # continuously looks for data????????????
+                    totalRecv += len(data)              # adds the newly received data to the count received
                     f.write(data)
-                    print ("{0:.2f}".format((totalRecv/float(filesize))*100+"% Done"))
+                    print ("{0:.2f}".format((totalRecv/float(filesize))*100+"% Done")) # cute loading message
                 print ("Download Complete!") 
         else:
             print ("File does not Exist!")
@@ -49,17 +52,33 @@ def get(socket):
         quit(socket)
 
 def putFile(socket):
-    print("PUT!")
+    cmd = pickle.dumps('PUT')
+    socket.send(cmd)                                    # sends the requested commandInput in bytes
+    data = socket.recv(1024)                            # receives data from command
+    bytesRecv = pickle.loads(data)                      # pickle "decodes" the pickeled file dumped by the server                     
+    return bytesRecv
 
 
 def multiget(socket):
-    print("MGET!")
+    cmd = pickle.dumps('MGET')
+    socket.send(cmd)                                    # sends the requested commandInput in bytes
+    data = socket.recv(1024)                            # receives data from command
+    bytesRecv = pickle.loads(data)                      # pickle "decodes" the pickeled file dumped by the server                     
+    return bytesRecv
 
 def multiput(socket):
-    print("MPUT!")
+    cmd = pickle.dumps('MPUT')
+    socket.send(cmd)                                    # sends the requested commandInput in bytes
+    data = socket.recv(1024)                            # receives data from command
+    bytesRecv = pickle.loads(data)                      # pickle "decodes" the pickeled file dumped by the server                     
+    return bytesRecv
 
 def changeDir(socket):
-    print("CD!")
+    cmd = pickle.dumps('CD')
+    socket.send(cmd)                                    # sends the requested commandInput in bytes
+    data = socket.recv(1024)                            # receives data from command
+    bytesRecv = pickle.loads(data)                      # pickle "decodes" the pickeled file dumped by the server                     
+    return bytesRecv
 
 def quit(socket):
     socket.close()
@@ -67,31 +86,27 @@ def quit(socket):
 
 
 def Main():
-    host = raw_input("Enter the IP address of your server: ") #Newer versions of python will have to use input
+    host = input("Enter the IP address of your server: ") # older versions of python will have to use raw_input
     #host = "169.254.145.232"                            # Todd's IP address, Personal IP: 10.20.120.61
     port  = 5000                                        # actual port 
 
     s = socket.socket()                                 # creates the "port" we use to connect
-                             # This connects to the server
-    
-    
-    #cmd = input(">")
 
-    s.connect((host,port))     
-    login = str.decode(s.recv(1024))
+    s.connect((host,port))                              # This connects to the server
+    login = str.decode(s.recv(1024))                    # Promted by server imedeately after connection 
     if login == "LOGIN":
-        loginName = raw_input("Please enter username: ")
+        loginName = input("Please enter username: ")    # Prompt to see if account is already created
         if loginName == "anon":
-            loginEmail = raw_input("Please enter your e-mail: ")
+            loginEmail = input("Please enter your e-mail: ")
             loginInfo = [loginName, loginEmail]
         else:
-            password = raw_input("Please enter your password: ")
+            password = input("Please enter your password: ")
             loginInfo = [loginName, password]
         data = pickle.dumps(loginInfo)
         s.send(data)
-
+                                                        # Prompt user?
     while True:
-        commandInput = raw_input(">") #Newer versions of python will have to use input
+        commandInput = input(">") 
         
         if commandInput == "ls":
             lis = ls(s)
